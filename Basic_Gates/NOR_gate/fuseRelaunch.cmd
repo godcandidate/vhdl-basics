6a1ede49b99bf332f3dcbbfe25aef22ef6b1017e
godcandidate <godcandidate101@gmail.com>
2023-08-12 22:56:26 +0000

    create fuseRelaunch.cmd

---
+++ Basic_Gates/NOR_gate/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/eddie/Documents/Xilinx projects/BasicGates/NOR_gate/nor_gate_test_isim_beh.exe" -prj "/home/eddie/Documents/Xilinx projects/BasicGates/NOR_gate/nor_gate_test_beh.prj" "work.nor_gate_test" 
